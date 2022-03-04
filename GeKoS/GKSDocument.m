84838aa0c70d2fee9eb9947bb9e67fa57fb5263f
Alex Popadich <popadich@gmail.com>
2022-03-04 11:57:41 -0700

    Move default drawing properties (line color, back color, fill color) to drawing view

--- GeKoS/GKSDocument.m
+++ GeKoS/GKSDocument.m
@@ -20,31 +20,6 @@
     self = [super init];
     if (self) {
         // Add your subclass-specific initialization here.
-        NSError* error;
-        NSColor* aColor;
-        
-        NSData* theData =[[NSUserDefaults standardUserDefaults] dataForKey:gksPrefBackgroundColor];
-        if (theData != nil) {
-            aColor = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class] fromData:theData error:&error];
-            if (error.code == noErr) {
-                self.backColor = aColor;
-            }
-        }
-        theData = [[NSUserDefaults standardUserDefaults] dataForKey:gksPrefFillColor];
-        if (theData != nil) {
-            aColor = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class] fromData:theData error:&error];
-            if (error.code == noErr) {
-                self.fillColor = aColor;
-            }
-            
-        }
-        theData = [[NSUserDefaults standardUserDefaults] dataForKey:gksPrefPenColor];
-        if (theData != nil) {
-            aColor = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class] fromData:theData error:&error];
-            if (error.code == noErr) {
-                self.lineColor = aColor;
-            }
-        }
 
     }
     return self;
@@ -53,15 +28,6 @@
 + (BOOL)autosavesInPlace {
     return YES;
 }
-
-/*
-- (NSString *)windowNibName {
-    // Override returning the nib file name of the document
-    // If you need to use a subclass of NSWindowController or if your document supports multiple NSWindowControllers, you should remove this method and override -makeWindowControllers instead.
-    return @"Document";
-}
-*/
-
 
 - (void)makeWindowControllers {
     GKSWindowController *windowController = [[GKSWindowController alloc] initWithWindowNibName:@"Document"];
--- GeKoS/GKSDrawingView.m
+++ GeKoS/GKSDrawingView.m
@@ -6,6 +6,7 @@
 //
 
 #import "GKSDrawingView.h"
+#import "GKSConstants.h"
 
 @interface GKSDrawingView () {
     NSColor *bluePrintBlueColor;
@@ -19,9 +20,35 @@
 @implementation GKSDrawingView
 
 - (void)awakeFromNib {
+    
+    NSError* error;
+    NSColor* aColor;
+    
+    NSData* theData =[[NSUserDefaults standardUserDefaults] dataForKey:gksPrefBackgroundColor];
+    if (theData != nil) {
+        aColor = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class] fromData:theData error:&error];
+        if (error.code == noErr) {
+            self.backgroundColor = aColor;
+        }
+    }
+    theData = [[NSUserDefaults standardUserDefaults] dataForKey:gksPrefFillColor];
+    if (theData != nil) {
+        aColor = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class] fromData:theData error:&error];
+        if (error.code == noErr) {
+            self.fillColor = aColor;
+        }
+        
+    }
+    theData = [[NSUserDefaults standardUserDefaults] dataForKey:gksPrefPenColor];
+    if (theData != nil) {
+        aColor = [NSKeyedUnarchiver unarchivedObjectOfClass:[NSColor class] fromData:theData error:&error];
+        if (error.code == noErr) {
+            self.lineColor = aColor;
+        }
+    }
+    
     bluePrintBlueColor = [NSColor colorWithRed:0.066 green:0.510 blue:0.910 alpha:1.0];
     maizeColor = [NSColor colorWithRed:1.0 green:203.0/255.0 blue:5.0/255.0 alpha:1.0];
-    self.backgroundColor = bluePrintBlueColor;
     _showSurface = NO;
     _itemLineWidth = 1.0;
 }
@@ -43,6 +70,16 @@
     [NSColor.whiteColor set];
     [boxPath setLineWidth:self.itemLineWidth];
     [boxPath stroke];
+    
+    CGFloat radius = 30.0;
+    CGFloat ox = (dirtyRect.size.width - radius) / 2.0;
+    CGFloat oy = (dirtyRect.size.height - radius) / 2.0;
+    NSRect ovalRect = NSMakeRect(ox, oy, radius, radius);
+    NSBezierPath *testDrawing = [NSBezierPath bezierPathWithOvalInRect:ovalRect];
+    [self.lineColor set];
+    [self.fillColor setFill];
+    [testDrawing fill];
+    [testDrawing stroke];
 
 }
 
